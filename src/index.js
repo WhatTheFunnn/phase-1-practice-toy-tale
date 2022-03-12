430cb0b5d3272317b4961219525cd907f8108aa4
Dan Souza <souza.music@outlook.com>
2022-03-12 13:47:17 -0800

    update

--- src/index.js
+++ src/index.js
@@ -12,4 +12,26 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  getToys()
 });
+
+function getToys() {
+  let toyList = "http://localhost:3000/toys"
+  const body = document.querySelector('#toy-collection')
+
+  fetch(toyList)
+    .then(response => response.json())
+    .then(toys => {
+      toys.forEach(toy => {
+        console.log(toy)
+        var myToy = JSON.stringify(toy)
+        const li = document.createElement('li')
+        li.innerText = toy.name
+        
+        
+        body.append(li)
+      })
+    })
+}
+
+[0].name
